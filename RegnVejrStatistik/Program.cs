a5276418702c023ae372f71cab8206c33bb27104
Phillip Marek Hansen <phil6162@elevcampus.dk>
2022-12-07 11:54:53 +0100

    Added Code

--- RegnVejrStatistik/Program.cs
+++ RegnVejrStatistik/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RegnVejrStatistik;
+
+class Program
+{
+    public static void Main()
+    {
+        List<double> list = new List<double>();
+        Console.Write("Indtast antallet af måleværdier: ");
+        int antalmåleværdier = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        for (int i = 0; i < antalmåleværdier; i++)
+        {
+            Console.Write($"Indtast måleværdi {i + 1}: ");
+            list.Add(Convert.ToDouble(Console.ReadLine()));
+            Console.Clear();
+           
+        }
+        Console.Clear();
+
+        Console.WriteLine($"Dit gennemsnit er: {list.Average()}"); 
+        Console.WriteLine($"Dit max er: {list.Max()}");
+        Console.WriteLine($"Dit min er: {list.Min()}");
+
+    }
+}
